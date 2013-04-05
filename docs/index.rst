a7502548860adccad37171d39ab9a821dc72f50a
whtsky <whtsky@me.com>
2013-04-06 00:50:14 +0800

    add docs

--- docs/index.rst
+++ docs/index.rst
@@ -3,20 +3,7 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to Flask-WeRoBot's documentation!
+Flask-WeRoBot
 =========================================
-
-Contents:
-
-.. toctree::
-   :maxdepth: 2
-
-
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
+Flask-WeRoBot 是一个帮助你将 WeRoBot 集成进 Flask 的插件。
 
--- flask_werobot.py
+++ flask_werobot.py
@@ -11,51 +11,53 @@
 __version__ = '0.1.0'
 
 
-import werobot
-from flask import Flask
+try:
+    import werobot
+    from flask import Flask
+except ImportError:
+    pass
+else:
+    class WeRoBot(werobot.WeRoBot):
+        def __init__(self, rule, app=None):
+            super(WeRoBot, self).__init__(token='temptoken')
+            self.rule = rule
+            if app is not None:
+                self.init_app(app)
+            else:
+                self._app = None
 
+        def init_app(self, app):
+            assert isinstance(app, Flask)
+            self._app = app
+            token = app.config.get('werobot_token')
 
-class WeRoBot(werobot.WeRoBot):
-    def __init__(self, rule, app=None):
-        super(WeRoBot, self).__init__(token='temptoken')
-        self.rule = rule
-        if app is not None:
-            self.init_app(app)
-        else:
-            self._app = None
+            from werobot.utils import check_token, check_signature
+            from werobot.parser import parse_user_msg
+            from werobot.reply import create_reply
 
-    def init_app(self, app):
-        assert isinstance(app, Flask)
-        self._app = app
-        token = app.config.get('werobot_token')
+            if not check_token(token):
+                raise AttributeError('%s is not a vaild token.' % token)
+            self.token = token
 
-        from werobot.utils import check_token, check_signature
-        from werobot.parser import parse_user_msg
-        from werobot.reply import create_reply
+            from flask import request, make_response
 
-        if not check_token(token):
-            raise AttributeError('%s is not a vaild token.' % token)
-        self.token = token
+            def handler():
+                if not check_signature(token,
+                                       request.args.get('timestamp', ''),
+                                       request.args.get('nonce', ''),
+                                       request.args.get('signature', '')):
+                    return 'Unvailed request.'
+                if request.method == 'GET':
+                    return request.args['echostr']
 
-        from flask import request, make_response, abort
+                body = request.data
+                message = parse_user_msg(body)
+                reply = self._get_reply(message)
+                if not reply:
+                    return ''
+                response = make_response(create_reply(reply, message=message))
+                response.headers['content_type'] = 'application/xml'
+                return response
 
-        def handler():
-            if not check_signature(token,
-                                   request.args.get('timestamp', ''),
-                                   request.args.get('nonce', ''),
-                                   request.args.get('signature', '')):
-                return 'Unvailed request.'
-            if request.method == 'GET':
-                return request.args['echostr']
-
-            body = request.data
-            message = parse_user_msg(body)
-            reply = self._get_reply(message)
-            if not reply:
-                return ''
-            response = make_response(create_reply(reply, message=message))
-            response.headers['content_type'] = 'application/xml'
-            return response
-
-        app.add_url_rule(self.rule, endpoint='werobot',
-                         view_func=handler, methods=['GET', 'POST'])
+            app.add_url_rule(self.rule, endpoint='werobot',
+                             view_func=handler, methods=['GET', 'POST'])
